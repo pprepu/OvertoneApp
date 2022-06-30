ca1b5c8b338ae4a5f15c29d50aaf4945e04b6e09
pprepu <pekka.prepula@gmail.com>
2022-07-01 01:20:28 +0300

    express for static content

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const { server } = require('./server')
+const app = express()
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -10,6 +11,10 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     console.log('error connecting to MongoDB:', error.message)
   })
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+server.applyMiddleware({app})
+
+app.use(express.static('build'))
+
+app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
